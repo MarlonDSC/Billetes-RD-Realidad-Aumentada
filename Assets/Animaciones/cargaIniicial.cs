08589418cd044addbd4e7c903b3922c289426fc3
Shoghy Martinez <shoghymartinez@gmail.com>
2022-08-19 20:54:49 -0400

    Pantalla de carga inicial
    Agregué la animación de la pantalla de carga inicial, una vez que esta termina cambia a la escena StartOff

---
+++ Assets/Animaciones/cargaIniicial.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cargaIniicial : MonoBehaviour
+{
+    void Start(){
+        StartCoroutine(accion());
+    }
+    IEnumerator accion(){
+        GetComponent<Animator>().Rebind();
+        GetComponent<Animator>().Play("Pantalla de carga de inicio");
+        yield return new WaitForSeconds(139f/60f);
+        SceneManager.LoadScene(1);
+    }
+}
