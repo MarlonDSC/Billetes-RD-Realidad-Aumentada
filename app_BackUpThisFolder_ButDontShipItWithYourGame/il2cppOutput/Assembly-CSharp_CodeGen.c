bf86d6d29e3561f50368aea2c7eae4a0e28db35c
emmanuelbeltre <beltrelgg2@gmail.com>
2022-08-20 22:57:25 -0400

    Modelos 3d texturizados en el anverso y reverso del billete de 500

---
+++ app_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,321 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void cargaIniicial::Start()
+extern void cargaIniicial_Start_m55C25E7AE5205FE60E7204C22565A168439694C2 (void);
+// 0x00000002 System.Collections.IEnumerator cargaIniicial::accion()
+extern void cargaIniicial_accion_m993856149FAB240E7BAD6BD7A8A4404DE15E52EA (void);
+// 0x00000003 System.Void cargaIniicial::.ctor()
+extern void cargaIniicial__ctor_m7A9C3C5E62817D9DD4D75E8D3719240F920445D6 (void);
+// 0x00000004 System.Void cargaIniicial/<accion>d__2::.ctor(System.Int32)
+extern void U3CaccionU3Ed__2__ctor_m23A796A9D574760A7271A5B171187EF7E7AF127F (void);
+// 0x00000005 System.Void cargaIniicial/<accion>d__2::System.IDisposable.Dispose()
+extern void U3CaccionU3Ed__2_System_IDisposable_Dispose_m1AAA038AF760C3D4D355ABB019E5B39A0FAA03D5 (void);
+// 0x00000006 System.Boolean cargaIniicial/<accion>d__2::MoveNext()
+extern void U3CaccionU3Ed__2_MoveNext_mF0357FD3642AAF6EE127132642A09532B10C16E3 (void);
+// 0x00000007 System.Object cargaIniicial/<accion>d__2::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CaccionU3Ed__2_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m33F7C8862EBFA358CF0CC441F6D08337AA1822D0 (void);
+// 0x00000008 System.Void cargaIniicial/<accion>d__2::System.Collections.IEnumerator.Reset()
+extern void U3CaccionU3Ed__2_System_Collections_IEnumerator_Reset_m33E59B2A061A005569053F5E3E3B707FB04DB352 (void);
+// 0x00000009 System.Object cargaIniicial/<accion>d__2::System.Collections.IEnumerator.get_Current()
+extern void U3CaccionU3Ed__2_System_Collections_IEnumerator_get_Current_m537BDA06175699692C7BE675118432D326014D04 (void);
+// 0x0000000A System.Void infoBillete::Start()
+extern void infoBillete_Start_m6160D81C0620E5A8E17BF52D891F94E500B75A53 (void);
+// 0x0000000B System.Void infoBillete::mostrarInfo()
+extern void infoBillete_mostrarInfo_mBFD90066E8CDB67910F1D930C3D1FB4A0205F04B (void);
+// 0x0000000C System.Void infoBillete::.ctor()
+extern void infoBillete__ctor_m510B640ED7CF869639F373F81C2CBC6582858284 (void);
+// 0x0000000D System.Void infoBillete::<Start>b__6_0()
+extern void infoBillete_U3CStartU3Eb__6_0_mA7809F42362E069974A6615BC8CAF90A15288D69 (void);
+// 0x0000000E System.Void regresarBillete::Start()
+extern void regresarBillete_Start_m21F107EC0D5EADAB65578FF750BF07A44213098C (void);
+// 0x0000000F System.Collections.IEnumerator regresarBillete::regresar()
+extern void regresarBillete_regresar_mF97A9C4AD3173A9B2AA1936275046AAA80E09806 (void);
+// 0x00000010 System.Void regresarBillete::.ctor()
+extern void regresarBillete__ctor_m39E12CC95F18FEAB6B215772C3695EA9E96EB98E (void);
+// 0x00000011 System.Void regresarBillete::<Start>b__3_0()
+extern void regresarBillete_U3CStartU3Eb__3_0_mDA67C266DAEF41A5EE0D6D556FECF40F52B163DA (void);
+// 0x00000012 System.Void regresarBillete/<regresar>d__4::.ctor(System.Int32)
+extern void U3CregresarU3Ed__4__ctor_m2EEF4488EFC8D877A9298CBC7CBB6C10AF8658CB (void);
+// 0x00000013 System.Void regresarBillete/<regresar>d__4::System.IDisposable.Dispose()
+extern void U3CregresarU3Ed__4_System_IDisposable_Dispose_mA51547E6B4623E911B838DF7425BCB2CBA91C109 (void);
+// 0x00000014 System.Boolean regresarBillete/<regresar>d__4::MoveNext()
+extern void U3CregresarU3Ed__4_MoveNext_m357C85964AE90804478C05A416882974632239F9 (void);
+// 0x00000015 System.Object regresarBillete/<regresar>d__4::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CregresarU3Ed__4_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m9DA70721310EEEE6AAEA3CD4F4B1F50F9DA8002B (void);
+// 0x00000016 System.Void regresarBillete/<regresar>d__4::System.Collections.IEnumerator.Reset()
+extern void U3CregresarU3Ed__4_System_Collections_IEnumerator_Reset_m7EC3BFCCB9DD46A94224411E14B3966F34D0C226 (void);
+// 0x00000017 System.Object regresarBillete/<regresar>d__4::System.Collections.IEnumerator.get_Current()
+extern void U3CregresarU3Ed__4_System_Collections_IEnumerator_get_Current_m4B502B05025EB74A931E7990A4BAD0C553CEF4C1 (void);
+// 0x00000018 System.Void ubicadorDePuntos::Update()
+extern void ubicadorDePuntos_Update_mBB64F278705E1E8613060CD53AC42C19537A6B0F (void);
+// 0x00000019 System.Void ubicadorDePuntos::.ctor()
+extern void ubicadorDePuntos__ctor_mE06C9CD49CFDBBA989F56AAB804C9757A9E329CF (void);
+// 0x0000001A System.Void zoomBillete::Start()
+extern void zoomBillete_Start_m22AA8E196633498DC9FD7C66A418CB8EC1F18ECC (void);
+// 0x0000001B System.Collections.IEnumerator zoomBillete::zoom(UnityEngine.Vector3,UnityEngine.Vector2)
+extern void zoomBillete_zoom_m265F4A64E36415DCC650CC9D6B439E2FE9B441D5 (void);
+// 0x0000001C System.Void zoomBillete::.ctor()
+extern void zoomBillete__ctor_mA93C0121BDDD1E5DEC6B529EE4316A3AA74443C5 (void);
+// 0x0000001D System.Void zoomBillete/<zoom>d__4::.ctor(System.Int32)
+extern void U3CzoomU3Ed__4__ctor_m29640331DA3D92CDFD29FC2FAF8CF70CA2A8D841 (void);
+// 0x0000001E System.Void zoomBillete/<zoom>d__4::System.IDisposable.Dispose()
+extern void U3CzoomU3Ed__4_System_IDisposable_Dispose_m6439EB2FBF486AA11F5E9BF3112513A97A15DA1E (void);
+// 0x0000001F System.Boolean zoomBillete/<zoom>d__4::MoveNext()
+extern void U3CzoomU3Ed__4_MoveNext_m515BDBD5323B0451542F9F974798BCCF94E0FE8E (void);
+// 0x00000020 System.Object zoomBillete/<zoom>d__4::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CzoomU3Ed__4_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m34C60F858E4044756598C42A832EAF712FB1FD66 (void);
+// 0x00000021 System.Void zoomBillete/<zoom>d__4::System.Collections.IEnumerator.Reset()
+extern void U3CzoomU3Ed__4_System_Collections_IEnumerator_Reset_m1FE9D494BDA04A11C1141FDD5AC497BC7008282C (void);
+// 0x00000022 System.Object zoomBillete/<zoom>d__4::System.Collections.IEnumerator.get_Current()
+extern void U3CzoomU3Ed__4_System_Collections_IEnumerator_get_Current_m52B1B703F0423388730833F80F0E49854BDDB05B (void);
+// 0x00000023 System.Void billeteInfo::Start()
+extern void billeteInfo_Start_m31F29755F6BFE6116C11976A68D1EBFDC2332051 (void);
+// 0x00000024 System.Void billeteInfo::accion()
+extern void billeteInfo_accion_m0F0E6998C8D9B7BCBA0AAA38A46F24473AB761AB (void);
+// 0x00000025 System.Void billeteInfo::.ctor()
+extern void billeteInfo__ctor_m7F66BC4D5E31986206DB94BE67E9FD23FDBEB15F (void);
+// 0x00000026 System.Void billeteInfo::<Start>b__4_0()
+extern void billeteInfo_U3CStartU3Eb__4_0_m42BE911F40BA817827D64224849E42CF31B39023 (void);
+// 0x00000027 System.Void Buttons::Start()
+extern void Buttons_Start_mDAD22B1E24D7761E4DA1006CD7210B82EBAE161A (void);
+// 0x00000028 System.Void Buttons::Update()
+extern void Buttons_Update_m40B6956AC28222E875D6ACF29A0A5D22DEE54650 (void);
+// 0x00000029 System.Void Buttons::carga_panel(UnityEngine.GameObject)
+extern void Buttons_carga_panel_mCDB87C8A3BCCB7324C13F4F6BD568498B5A48BAE (void);
+// 0x0000002A System.Void Buttons::carga_billete50()
+extern void Buttons_carga_billete50_m29E53AA2EEE334741DE59F56A095924E7A0FCDC3 (void);
+// 0x0000002B System.Void Buttons::carga_billete100()
+extern void Buttons_carga_billete100_m1DD6CD4FCDD599A030B505A7EEB130F601E11F8C (void);
+// 0x0000002C System.Void Buttons::carga_billete200()
+extern void Buttons_carga_billete200_mB55533779B5212C6DC71EABB4DBF5E30779D893A (void);
+// 0x0000002D System.Void Buttons::carga_billete500()
+extern void Buttons_carga_billete500_mB55056970BCE9B991AB07459AF09DF3FF8DD06E1 (void);
+// 0x0000002E System.Void Buttons::atras()
+extern void Buttons_atras_mB833B6A3DEAEC49EE69A51D0B89AEA7374A01D1B (void);
+// 0x0000002F System.Void Buttons::.ctor()
+extern void Buttons__ctor_m3217EFBF53FE9D652EF123BDA4A06F98E196441D (void);
+// 0x00000030 System.Void CambioEscenas::Start()
+extern void CambioEscenas_Start_m2F29EC3F2E1D3A22A1D1DA818B84CDB1EFD3C148 (void);
+// 0x00000031 System.Void CambioEscenas::cargarEscena()
+extern void CambioEscenas_cargarEscena_m8AAFDD598CEA5B0B43180F3FFBAF535EEF68BBCB (void);
+// 0x00000032 System.Void CambioEscenas::.ctor()
+extern void CambioEscenas__ctor_mB27061F4D462EF86D8A584A7C06408B04533353A (void);
+// 0x00000033 System.Void displayBilleteInfo::Start()
+extern void displayBilleteInfo_Start_mCDDDF0FE25459C38E9BAEDD5EB6C25925875248C (void);
+// 0x00000034 System.Void displayBilleteInfo::.ctor()
+extern void displayBilleteInfo__ctor_mB22E5B9AC2240011BF855A820A255C157864E94B (void);
+// 0x00000035 System.Void infoCarrier::Start()
+extern void infoCarrier_Start_m7FDAD6B0A1A4AF5758ADE01C9A72118464BC95B8 (void);
+// 0x00000036 System.Void infoCarrier::destruir()
+extern void infoCarrier_destruir_mA0002998DEB222F5E5A6AAEC1F44CD75490D7A22 (void);
+// 0x00000037 System.Void infoCarrier::.ctor()
+extern void infoCarrier__ctor_m92BA220184733A4669533186203DB370E1DDA9D5 (void);
+// 0x00000038 System.Void RotacionBilletes::Start()
+extern void RotacionBilletes_Start_m33A14FC0500A47AA9FE292E9457CD71C022D1537 (void);
+// 0x00000039 System.Collections.IEnumerator RotacionBilletes::rotador()
+extern void RotacionBilletes_rotador_mBC301DBB9B7FD3CAEE9E2DF97AB34CFCFA161708 (void);
+// 0x0000003A System.Void RotacionBilletes::.ctor()
+extern void RotacionBilletes__ctor_m709CB94288B79E905E2B704A5E1AB91D21864639 (void);
+// 0x0000003B System.Void RotacionBilletes::<Start>b__4_0()
+extern void RotacionBilletes_U3CStartU3Eb__4_0_mB9CD6777DF0E9AE97F5ECF79D3A4161E2E2E6D3D (void);
+// 0x0000003C System.Void RotacionBilletes/<rotador>d__5::.ctor(System.Int32)
+extern void U3CrotadorU3Ed__5__ctor_mAEB41619C9184B0F6293B3E8741F010E1CC7EE49 (void);
+// 0x0000003D System.Void RotacionBilletes/<rotador>d__5::System.IDisposable.Dispose()
+extern void U3CrotadorU3Ed__5_System_IDisposable_Dispose_mBA509C5D57D7A6C60831E02B62FCA238D6866710 (void);
+// 0x0000003E System.Boolean RotacionBilletes/<rotador>d__5::MoveNext()
+extern void U3CrotadorU3Ed__5_MoveNext_mB88253F0693EFE5CCB914DFB2B3100C71265856C (void);
+// 0x0000003F System.Object RotacionBilletes/<rotador>d__5::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CrotadorU3Ed__5_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mD10A20E7D070EF7DCE6DF9E3441E4EA6B3425D71 (void);
+// 0x00000040 System.Void RotacionBilletes/<rotador>d__5::System.Collections.IEnumerator.Reset()
+extern void U3CrotadorU3Ed__5_System_Collections_IEnumerator_Reset_m8125A4174CBCC45BC3B84B6CA69641054835C6E2 (void);
+// 0x00000041 System.Object RotacionBilletes/<rotador>d__5::System.Collections.IEnumerator.get_Current()
+extern void U3CrotadorU3Ed__5_System_Collections_IEnumerator_get_Current_m61CF582881A2A5885B05B92C435B5C83FB37FDB7 (void);
+// 0x00000042 System.Void swipe::Start()
+extern void swipe_Start_m43627CF8173E564D19A7100435DA93EE6EB4F001 (void);
+// 0x00000043 System.Void swipe::Update()
+extern void swipe_Update_mDC30B95D2DF07EA419CBF4995F8BCD21871EE42F (void);
+// 0x00000044 System.Void swipe::GecisiDuzenle(System.Single,System.Single[],UnityEngine.UI.Button)
+extern void swipe_GecisiDuzenle_mFAC4EE62C2C9DBFA03FBD3C3CC9D7A0ADA7A4C53 (void);
+// 0x00000045 System.Void swipe::WhichBtnClicked(UnityEngine.UI.Button)
+extern void swipe_WhichBtnClicked_mF90E5DDFFB7449AF72CA15C9E6B33A7E81B2AFE0 (void);
+// 0x00000046 System.Void swipe::.ctor()
+extern void swipe__ctor_mB669B78238F62B2619910E782B5C4BB5B9154FB3 (void);
+static Il2CppMethodPointer s_methodPointers[70] = 
+{
+	cargaIniicial_Start_m55C25E7AE5205FE60E7204C22565A168439694C2,
+	cargaIniicial_accion_m993856149FAB240E7BAD6BD7A8A4404DE15E52EA,
+	cargaIniicial__ctor_m7A9C3C5E62817D9DD4D75E8D3719240F920445D6,
+	U3CaccionU3Ed__2__ctor_m23A796A9D574760A7271A5B171187EF7E7AF127F,
+	U3CaccionU3Ed__2_System_IDisposable_Dispose_m1AAA038AF760C3D4D355ABB019E5B39A0FAA03D5,
+	U3CaccionU3Ed__2_MoveNext_mF0357FD3642AAF6EE127132642A09532B10C16E3,
+	U3CaccionU3Ed__2_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m33F7C8862EBFA358CF0CC441F6D08337AA1822D0,
+	U3CaccionU3Ed__2_System_Collections_IEnumerator_Reset_m33E59B2A061A005569053F5E3E3B707FB04DB352,
+	U3CaccionU3Ed__2_System_Collections_IEnumerator_get_Current_m537BDA06175699692C7BE675118432D326014D04,
+	infoBillete_Start_m6160D81C0620E5A8E17BF52D891F94E500B75A53,
+	infoBillete_mostrarInfo_mBFD90066E8CDB67910F1D930C3D1FB4A0205F04B,
+	infoBillete__ctor_m510B640ED7CF869639F373F81C2CBC6582858284,
+	infoBillete_U3CStartU3Eb__6_0_mA7809F42362E069974A6615BC8CAF90A15288D69,
+	regresarBillete_Start_m21F107EC0D5EADAB65578FF750BF07A44213098C,
+	regresarBillete_regresar_mF97A9C4AD3173A9B2AA1936275046AAA80E09806,
+	regresarBillete__ctor_m39E12CC95F18FEAB6B215772C3695EA9E96EB98E,
+	regresarBillete_U3CStartU3Eb__3_0_mDA67C266DAEF41A5EE0D6D556FECF40F52B163DA,
+	U3CregresarU3Ed__4__ctor_m2EEF4488EFC8D877A9298CBC7CBB6C10AF8658CB,
+	U3CregresarU3Ed__4_System_IDisposable_Dispose_mA51547E6B4623E911B838DF7425BCB2CBA91C109,
+	U3CregresarU3Ed__4_MoveNext_m357C85964AE90804478C05A416882974632239F9,
+	U3CregresarU3Ed__4_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m9DA70721310EEEE6AAEA3CD4F4B1F50F9DA8002B,
+	U3CregresarU3Ed__4_System_Collections_IEnumerator_Reset_m7EC3BFCCB9DD46A94224411E14B3966F34D0C226,
+	U3CregresarU3Ed__4_System_Collections_IEnumerator_get_Current_m4B502B05025EB74A931E7990A4BAD0C553CEF4C1,
+	ubicadorDePuntos_Update_mBB64F278705E1E8613060CD53AC42C19537A6B0F,
+	ubicadorDePuntos__ctor_mE06C9CD49CFDBBA989F56AAB804C9757A9E329CF,
+	zoomBillete_Start_m22AA8E196633498DC9FD7C66A418CB8EC1F18ECC,
+	zoomBillete_zoom_m265F4A64E36415DCC650CC9D6B439E2FE9B441D5,
+	zoomBillete__ctor_mA93C0121BDDD1E5DEC6B529EE4316A3AA74443C5,
+	U3CzoomU3Ed__4__ctor_m29640331DA3D92CDFD29FC2FAF8CF70CA2A8D841,
+	U3CzoomU3Ed__4_System_IDisposable_Dispose_m6439EB2FBF486AA11F5E9BF3112513A97A15DA1E,
+	U3CzoomU3Ed__4_MoveNext_m515BDBD5323B0451542F9F974798BCCF94E0FE8E,
+	U3CzoomU3Ed__4_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m34C60F858E4044756598C42A832EAF712FB1FD66,
+	U3CzoomU3Ed__4_System_Collections_IEnumerator_Reset_m1FE9D494BDA04A11C1141FDD5AC497BC7008282C,
+	U3CzoomU3Ed__4_System_Collections_IEnumerator_get_Current_m52B1B703F0423388730833F80F0E49854BDDB05B,
+	billeteInfo_Start_m31F29755F6BFE6116C11976A68D1EBFDC2332051,
+	billeteInfo_accion_m0F0E6998C8D9B7BCBA0AAA38A46F24473AB761AB,
+	billeteInfo__ctor_m7F66BC4D5E31986206DB94BE67E9FD23FDBEB15F,
+	billeteInfo_U3CStartU3Eb__4_0_m42BE911F40BA817827D64224849E42CF31B39023,
+	Buttons_Start_mDAD22B1E24D7761E4DA1006CD7210B82EBAE161A,
+	Buttons_Update_m40B6956AC28222E875D6ACF29A0A5D22DEE54650,
+	Buttons_carga_panel_mCDB87C8A3BCCB7324C13F4F6BD568498B5A48BAE,
+	Buttons_carga_billete50_m29E53AA2EEE334741DE59F56A095924E7A0FCDC3,
+	Buttons_carga_billete100_m1DD6CD4FCDD599A030B505A7EEB130F601E11F8C,
+	Buttons_carga_billete200_mB55533779B5212C6DC71EABB4DBF5E30779D893A,
+	Buttons_carga_billete500_mB55056970BCE9B991AB07459AF09DF3FF8DD06E1,
+	Buttons_atras_mB833B6A3DEAEC49EE69A51D0B89AEA7374A01D1B,
+	Buttons__ctor_m3217EFBF53FE9D652EF123BDA4A06F98E196441D,
+	CambioEscenas_Start_m2F29EC3F2E1D3A22A1D1DA818B84CDB1EFD3C148,
+	CambioEscenas_cargarEscena_m8AAFDD598CEA5B0B43180F3FFBAF535EEF68BBCB,
+	CambioEscenas__ctor_mB27061F4D462EF86D8A584A7C06408B04533353A,
+	displayBilleteInfo_Start_mCDDDF0FE25459C38E9BAEDD5EB6C25925875248C,
+	displayBilleteInfo__ctor_mB22E5B9AC2240011BF855A820A255C157864E94B,
+	infoCarrier_Start_m7FDAD6B0A1A4AF5758ADE01C9A72118464BC95B8,
+	infoCarrier_destruir_mA0002998DEB222F5E5A6AAEC1F44CD75490D7A22,
+	infoCarrier__ctor_m92BA220184733A4669533186203DB370E1DDA9D5,
+	RotacionBilletes_Start_m33A14FC0500A47AA9FE292E9457CD71C022D1537,
+	RotacionBilletes_rotador_mBC301DBB9B7FD3CAEE9E2DF97AB34CFCFA161708,
+	RotacionBilletes__ctor_m709CB94288B79E905E2B704A5E1AB91D21864639,
+	RotacionBilletes_U3CStartU3Eb__4_0_mB9CD6777DF0E9AE97F5ECF79D3A4161E2E2E6D3D,
+	U3CrotadorU3Ed__5__ctor_mAEB41619C9184B0F6293B3E8741F010E1CC7EE49,
+	U3CrotadorU3Ed__5_System_IDisposable_Dispose_mBA509C5D57D7A6C60831E02B62FCA238D6866710,
+	U3CrotadorU3Ed__5_MoveNext_mB88253F0693EFE5CCB914DFB2B3100C71265856C,
+	U3CrotadorU3Ed__5_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mD10A20E7D070EF7DCE6DF9E3441E4EA6B3425D71,
+	U3CrotadorU3Ed__5_System_Collections_IEnumerator_Reset_m8125A4174CBCC45BC3B84B6CA69641054835C6E2,
+	U3CrotadorU3Ed__5_System_Collections_IEnumerator_get_Current_m61CF582881A2A5885B05B92C435B5C83FB37FDB7,
+	swipe_Start_m43627CF8173E564D19A7100435DA93EE6EB4F001,
+	swipe_Update_mDC30B95D2DF07EA419CBF4995F8BCD21871EE42F,
+	swipe_GecisiDuzenle_mFAC4EE62C2C9DBFA03FBD3C3CC9D7A0ADA7A4C53,
+	swipe_WhichBtnClicked_mF90E5DDFFB7449AF72CA15C9E6B33A7E81B2AFE0,
+	swipe__ctor_mB669B78238F62B2619910E782B5C4BB5B9154FB3,
+};
+static const int32_t s_InvokerIndices[70] = 
+{
+	6999,
+	6866,
+	6999,
+	5497,
+	6999,
+	6774,
+	6866,
+	6999,
+	6866,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	6866,
+	6999,
+	6999,
+	5497,
+	6999,
+	6774,
+	6866,
+	6999,
+	6866,
+	6999,
+	6999,
+	6999,
+	2457,
+	6999,
+	5497,
+	6999,
+	6774,
+	6866,
+	6999,
+	6866,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	5526,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	6999,
+	6866,
+	6999,
+	6999,
+	5497,
+	6999,
+	6774,
+	6866,
+	6999,
+	6866,
+	6999,
+	6999,
+	1698,
+	5526,
+	6999,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	70,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
