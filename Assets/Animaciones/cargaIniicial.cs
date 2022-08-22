88090dae3a5390eb65275e8052f86317dc06151b
WithThaMiguel <106761660+WithThaMiguel@users.noreply.github.com>
2022-08-22 01:59:57 -0400

    Added animation to the scenes

--- Assets/Animaciones/cargaIniicial.cs
+++ Assets/Animaciones/cargaIniicial.cs
@@ -1,18 +1,18 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class cargaIniicial : MonoBehaviour
 {
-    public string scene;
-    void Start(){
-        StartCoroutine(accion());
-    }
-    IEnumerator accion(){
-        GetComponent<Animator>().Rebind();
-        GetComponent<Animator>().Play("Pantalla de carga de inicio");
-        yield return new WaitForSeconds(139f/60f);
-        SceneManager.LoadScene(scene);
-    }
+	public string scene;
+	void Start(){
+		StartCoroutine(accion());
+	}
+	IEnumerator accion(){
+		GetComponent<Animator>().Rebind();
+		GetComponent<Animator>().Play("Pantalla de carga de inicio");
+		yield return new WaitForSeconds(139f/60f);
+		SceneManager.LoadScene(scene);
+	}
 }
--- Assets/Scripts/CambioEscenas.cs
+++ Assets/Scripts/CambioEscenas.cs
@@ -8,14 +8,18 @@ using UnityEngine.SceneManagement;
 public class CambioEscenas : MonoBehaviour
 {
 	public string NombreEscena;
+
     
 	// Start is called before the first frame update
 	void Start()
 	{
 		GetComponent<Button>().onClick.AddListener(cargarEscena);
-	}
+		}
     
 	void cargarEscena(){
 		SceneManager.LoadScene(NombreEscena);
 	}
+	
+	
+	
 }
