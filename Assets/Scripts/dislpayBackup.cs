71f75525adace1fb2c7db6974ecf467b6c4dd795
WithThaMiguel <106761660+WithThaMiguel@users.noreply.github.com>
2022-08-25 17:08:24 -0400

    Texts changed

--- Assets/Scripts/dislpayBackup.cs
+++ Assets/Scripts/dislpayBackup.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
-public class displayBilleteInfo : MonoBehaviour
+public class displayBackup : MonoBehaviour
 {
-    public Text titulo;
-    public Text descripcion;
+    public TextMeshProUGUI titulo;
+    public TextMeshProUGUI descripcion;
     public Image imagen;
     public CambioEscenas boton;
-    void Start(){
+    void Start()
+    {
         infoCarrier openChest = GameObject.Find("infoCarrier").GetComponent<infoCarrier>();
         titulo.text = openChest.titulo;
         descripcion.text = openChest.descripcion;
@@ -17,11 +18,14 @@ public class displayBilleteInfo : MonoBehaviour
 
         Sprite parteBillete = openChest.imagen;
         Vector2 sizePadre = imagen.transform.parent.GetComponent<RectTransform>().sizeDelta;
-        if(parteBillete.rect.width > parteBillete.rect.height){
-            float height = sizePadre.x*((float)parteBillete.rect.height/(float)parteBillete.rect.width);
+        if (parteBillete.rect.width > parteBillete.rect.height)
+        {
+            float height = sizePadre.x * ((float)parteBillete.rect.height / (float)parteBillete.rect.width);
             imagen.rectTransform.sizeDelta = new Vector2(sizePadre.x, height);
-        }else{
-            float width = sizePadre.y*((float)parteBillete.rect.width/(float)parteBillete.rect.height);
+        }
+        else
+        {
+            float width = sizePadre.y * ((float)parteBillete.rect.width / (float)parteBillete.rect.height);
             imagen.rectTransform.sizeDelta = new Vector2(width, sizePadre.y);
         }
         imagen.transform.parent.GetChild(1).GetComponent<RectTransform>().sizeDelta = new Vector2(imagen.rectTransform.sizeDelta.x + 20f, imagen.rectTransform.sizeDelta.y + 20f);
