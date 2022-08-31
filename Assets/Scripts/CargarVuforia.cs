38e6d80e955dbe68a102e45d5739c00024b3b426
Brailyn Dominguez <brailynmiguel27@gmail.com>
2022-08-31 16:10:53 -0400

    Pantalla de carga creada
    La misma era necesaria para evitar la pantalla oscura al momento de abrir la app y cargar el vuforia

---
+++ Assets/Scripts/CargarVuforia.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using Vuforia.UnityRuntimeCompiled;
+
+public class CargarVuforia : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	    VuforiaApplication.Instance.Initialize();
+	    //GetComponent<VuforiaBehaviour>().enabled = true;
+	    //GetComponent<DefaultInitializationErrorHandler>().enabled = true;
+	 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	  
+    }
+}
---
+++ Assets/Scripts/PantallaCarga.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PantallaCarga : MonoBehaviour
+{	
+	public string nombreScena;
+    // Start is called before the first frame update
+    void Start()
+    {   
+	    StartCoroutine(CargarAsync(nombreScena));    
+    }
+
+    // Update is called once per frame
+	void Update()
+    
+	{
+
+    }
+
+	//Co-rutina para cargar la escena de forma Asincrona
+	IEnumerator CargarAsync(string nombreScena){
+		yield return new WaitForSeconds(2f);
+		
+		AsyncOperation Operacion = SceneManager.LoadSceneAsync(nombreScena);
+		
+		// Espera hasta que la escena este completamente cargada
+		while (!Operacion.isDone)
+		{
+			yield return null;
+		}
+	}
+	
+}
