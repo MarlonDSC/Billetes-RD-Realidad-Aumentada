18171a56b77b624ade2a11d3079d790172c001cc
emmanuelbeltre <beltrelgg2@gmail.com>
2022-09-04 09:05:14 -0400

    Audio Script mejorado

--- Assets/Prefabs/AudioPlay.cs
+++ Assets/Prefabs/AudioPlay.cs
@@ -2,13 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent (typeof(AudioSource))]
 public class AudioPlay : MonoBehaviour
 {
-    public AudioClip miAudio;
-    public Camera miCamara;
+    public AudioClip audioParaReproducir;
+    public GameObject miModelo3D;
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = audioParaReproducir;
+
+        audioSource.playOnAwake = false;
+    }
+    private void Start()
+    {
+        audioSource.clip = audioParaReproducir;
+    }
 
     void iniciarSonido()
     {
-        AudioSource.PlayClipAtPoint(miAudio, miCamara.transform.position, 0.5f);
+        //AudioSource.PlayClipAtPoint(miAudio, miCamara.transform.position, 0.5f);
+
+        audioSource.Play();
+        if(miModelo3D.activeSelf != true)  audioSource.Stop();
     }
 }
