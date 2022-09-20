983721f392303fe79c460e8d18739286c263eff8
Brailyn Dominguez <brailynmiguel27@gmail.com>
2022-09-20 10:03:34 -0400

    script para bloquear el modelo escalado

--- Assets/Scripts/LockUnlock.cs
+++ Assets/Scripts/LockUnlock.cs
@@ -6,14 +6,22 @@ using TMPro;
 
 public class LockUnlock : MonoBehaviour
 {
+    public GameObject ArCamera;
+    public GameObject Empty;
     public Button Boton;
     public GameObject prefab;
+    public GameObject ImageTarget;
     public GameObject newObject;
+
+    public Vector3 PositonModel;
+    public Quaternion RotationModel;
     public bool Estado = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        PositonModel = prefab.transform.position;
+        RotationModel = prefab.transform.rotation;
         if (Estado == true)
         {
             Boton.onClick.AddListener(delegate { CrearPrefab(); });
@@ -33,8 +41,25 @@ public class LockUnlock : MonoBehaviour
 
     public void CrearPrefab()
     {
-       newObject = Instantiate(prefab);
-       Estado = false;
+        Debug.Log("rotation: " + prefab.transform.rotation);
+        Debug.Log("position: "+prefab.transform.position);
+
+        newObject = Instantiate(prefab);
+        
+        newObject.transform.parent = Empty.transform;
+
+
+        Empty.transform.position = ImageTarget.transform.position; 
+        Empty.transform.rotation = ImageTarget.transform.rotation;
+
+        newObject.transform.position = prefab.transform.position;
+        newObject.transform.rotation = prefab.transform.rotation;
+
+
+        Empty.transform.parent = ArCamera.transform;
+
+        ImageTarget.SetActive(false);
+        Estado = false;
     }
     public void DestruirPrefab()
     {
