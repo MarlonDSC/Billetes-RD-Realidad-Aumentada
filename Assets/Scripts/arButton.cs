9c307b1263a03057370fe18db2160654abc2dad6
Shoghy Martinez <shoghymartinez@gmail.com>
2022-09-30 20:24:50 -0400

    Fin de la actualización de la versión web
    Todos los cambios de la version mobile han sido añadidos a la version web, se necesita probar por cualquier bug

--- Assets/Scripts/arButton.cs
+++ Assets/Scripts/arButton.cs
@@ -8,7 +8,8 @@ public class arButton : MonoBehaviour
 {
     public Camera mainCamera;
     public RotacionBilletes btnRotacion;
-    public Animator btnVolver, btnRotar, titulo, btnAR;
+    public Animator btnVolver, btnRotar;
+    public Text titulo;
     void Start(){
         GetComponent<Button>().onClick.AddListener(() => StartCoroutine(accion()));
     }
@@ -22,15 +23,25 @@ public class arButton : MonoBehaviour
         infoCarrier chest = GameObject.Find("infoCarrier").GetComponent<infoCarrier>();
         chest.position = caraDelantera.activeSelf ? caraDelantera.transform.localPosition : caraTrasera.transform.localPosition;
 
-        Animator[] animations = {btnVolver, btnRotar, titulo, btnAR};
+        Animator[] animations = {btnVolver, btnRotar, GetComponent<Animator>()};
         foreach(Animator animacion in animations){
             animacion.Rebind();
         }
         btnVolver.Play("btnAR");
         btnRotar.Play("btnAR");
-        titulo.Play("title");
-        btnAR.Play("btnAR");
-        yield return new WaitForSeconds(0.5f);
+        GetComponent<Animator>().Play("btnAR");
+        
+        string txtTitle = titulo.text;
+        float tiempo = 0f;
+        while(tiempo < 0.5f){
+            tiempo+= Time.deltaTime;
+            if(tiempo < 0.5f){
+                titulo.text = txtTitle.Substring(0, txtTitle.Length - (int)((float)txtTitle.Length * tiempo/0.5f));
+            }else{
+                titulo.text = "";
+            }
+            yield return null;
+        }
 
         scene.allowSceneActivation = true;
     }
